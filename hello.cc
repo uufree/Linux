cda8caafeb1c799fc53dccaa74c6c2d8a3d3b32b
uufree <369470777@qq.com>
2017-02-18 13:25:23 +0800

    one

---
+++ hello.cc
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: hello.cc
+	> Author: uuchen
+	> Mail: 1319081676@qq.com
+	> Created Time: 2017年02月18日 星期六 13时24分12秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main(int rargc,char** argv)
+{
+    cout << "hello wrold!"  << endl;
+    return 0;
+}
