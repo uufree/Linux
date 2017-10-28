5ab62d5e78709b2f8076fec60bdab9766c41f539
uufree <369470777@qq.com>
2017-10-28 12:55:13 +0800

    fix hello.cc

---
+++ hello.cc
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: hello.cc
+	> Author: uuchen
+	> Mail: 1319081676@qq.com
+	> Created Time: 2017年10月28日 星期六 12时49分05秒
+ ************************************************************************/
+
+#include<iostream>
+
+int main(int argc,char** argv)
+{
+    std::cout << "hello,git!" << std::endl;
+    std::cout << "uuchen" << std::endl;
+
+    return 0;
+}
